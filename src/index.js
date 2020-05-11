7af930becdb1a1800cf71c03a19a0e85cb5fa4b0
Nahla Shiri <nahla.job@gmail.com>
2020-05-11 16:21:07 +0100

    correction index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Profile from './profile/profile.js';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Profile  fullName="Mahatma Gandhi" profession="Indian lawyer" bio="Born and raised in a Hindu family in coastal Gujarat, western India, Gandhi was trained in law at the Inner Temple, London, and called to the bar at age 22 in June 1891. After two uncertain years in India, where he was unable to start a successful law practice, he moved to South Africa in 1893 to represent an Indian merchant in a lawsuit. ">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Mahatma-Gandhi%2C_studio%2C_1931.jpg/330px-Mahatma-Gandhi%2C_studio%2C_1931.jpg" alt="Mahatma Gandhi" />
-    </Profile>
-    
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
